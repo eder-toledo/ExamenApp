988043acbe104646353bc286f7b0e10bd0606127
Eder Toledo <me890506@gmail.com>
2016-11-13 21:45:32 -0600

    Se agregaron relaciones users, datosAcademicos y examen, se incluyo relacion con datosAcademicos y estado al userRoute

--- ExamWeb/Models/index.js
+++ ExamWeb/Models/index.js
@@ -8,7 +8,7 @@ var sequelize = new Sequelize('examenes', 'root', '',{
 
 var models = require('sequelize-auto-import')(sequelize, '');
 
-//Relación materia - grado
+//Relación materias - grado
 models.ex_Materia.belongsTo(models.ex_Grado, {
     foreignKey: 'ex_Grado_idGrado'
 });
@@ -17,7 +17,7 @@ models.ex_Grado.hasMany(models.ex_Materia, {
     foreignKey: 'ex_Grado_idGrado'
 });
 
-//Relacion usuario - estado
+//Relacion usuarios - estado
 models.ex_User.belongsTo(models.ex_Estado, {
     foreignKey: 'ex_Estado_idEstado'
 });
@@ -26,13 +26,67 @@ models.ex_Estado.hasMany(models.ex_User, {
     foreignKey: 'ex_Estado_idEstado'
 });
 
-//Relacion datos academicos - usuario
+//Relacion datosAcademicos - usuario
 models.ex_DatosAcademicos.belongsTo(models.ex_User, {
     foreignKey: 'ex_user_idUser'
 });
 
 models.ex_User.hasOne(models.ex_DatosAcademicos, {
     foreignKey: 'ex_user_idUser'
+});
+
+//Relacion datosAcademicos - carrera
+models.ex_DatosAcademicos.belongsTo(models.ex_Carrera, {
+    foreignKey: 'ex_Carrera_idCarrera'
+});
+
+models.ex_Carrera.hasMany(models.ex_DatosAcademicos, {
+    foreignKey: 'ex_Carrera_idCarrera'
+});
+
+//Relacion datosAcademicos - gradoAcademico
+models.ex_DatosAcademicos.belongsTo(models.ex_GradoAcademico, {
+    foreignKey: 'ex_Grado_idGrado'
+});
+
+models.ex_GradoAcademico.hasMany(models.ex_DatosAcademicos, {
+    foreignKey: 'ex_Grado_idGrado'
+});
+
+//Relacion estados - pais
+models.ex_Estado.belongsTo(models.ex_Pais, {
+    foreignKey: 'ex_Pais_idpais'
+});
+
+models.ex_Pais.hasMany(models.ex_Estado, {
+    foreignKey: 'ex_Pais_idpais'
+});
+
+//Relacion examenes - usuario
+models.ex_Examen.belongsTo(models.ex_User, {
+    foreignKey: 'ex_User_idUser'
+});
+
+models.ex_User.hasMany(models.ex_Examen, {
+    foreignKey: 'ex_User_idUser';
+});
+
+//Relacion examenes - periodo
+models.ex_Examen.belongsTo(models.ex_Periodo, {
+    foreignKey: 'ex_Periodo_idPeriodo'
+});
+
+models.ex_Periodo.hasMany(models.ex_Examen, {
+    foreignKey: 'ex_Periodo_idPeriodo'
+});
+
+//Relacion examenes - materia
+models.ex_Examen.belongsTo(models.ex_Materia, {
+    foreignKey: ex_Materia_idMateria
+});
+
+models.ex_Materia.hasMany(models.ex_Examen, {
+    foreignKey: 'ex_Materia_idMateria'
 });
 
 module.exports = models;
--- ExamWeb/Routes/usersRoute.js
+++ ExamWeb/Routes/usersRoute.js
@@ -1,14 +1,22 @@
 ﻿usersRoute = function (server, db, Sequelize, apiUrl) {
     urlRoute = apiUrl + 'users/';
 
+    var sql = {};
+    sql.where = { estatus: "Activo"};
+    sql.attributes = { exclude: ['createdAt', 'updatedAt', 'estatus', 'password', 'ex_Estado_idEstado'] };
+
     function allUsers(req, res, next) {
-        db.ex_User.findAll({
-        }).then(function (users) {
+        db.ex_User.findAll(sql).then(function (users) {
             var data = {};
             if (!users) {
-                data.error = "true";
+                data.status = "error";
+                data.code = "ElementNotFound";
+                data.message = "User with id " + req.params.id + " does not exist";
             } else {
+                data.status = "success"
+                data.code = "ResultsForSearch"
                 data.data = users;
+                data.count = users.length;
             }
             res.send(data);
             next();
@@ -16,17 +24,43 @@
     }
 
     function usersById(req, res, next) {
-        db.ex_User.find({
-            where: {
-                idUser: req.params.id
-            },
-            include: [db.ex_DatosAcademicos]
-        }).then(function (users) {
+        if (req.params.include != null) {
+            sql.include = [];
+            relations = req.params.include.split("_");
+            for (i = 0; i < relations.length; i++){
+                if (relations[i] == "datosAcademicos") {
+                    sql.include[i] = {};
+                    sql.include[i].model = db.ex_DatosAcademicos;
+                    sql.include[i].attributes = { exclude: ['createdAt', 'updatedAt', 'ex_Grado_idGrado', 'ex_Carrera_idCarrera', 'ex_user_idUser'] };
+                    sql.include[i].include = [];
+                    sql.include[i].include[0] = {};
+                    sql.include[i].include[0].model = db.ex_Carrera;
+                    sql.include[i].include[0].attributes = { exclude: ['createdAt', 'updatedAt'] };
+                    sql.include[i].include[1] = {};
+                    sql.include[i].include[1].model = db.ex_GradoAcademico;
+                    sql.include[i].include[1].attributes = { exclude: ['createdAt', 'updatedAt'] };
+                } else if (relations[i] == "estado") {
+                    sql.include[i] = {};
+                    sql.include[i].model = db.ex_Estado;
+                    sql.include[i].attributes = { exclude: ['createdAt', 'updatedAt', 'ex_Pais_idpais'] };
+                    sql.include[i].include = [];
+                    sql.include[i].include[0] = {};
+                    sql.include[i].include[0].model = db.ex_Pais;
+                    sql.include[i].include[0].attributes = { exclude: ['createdAt', 'updatedAt'] };
+                }
+            }
+        }
+        db.ex_User.find(sql).then(function (users) {
             data = {};
             if (!users) {
-                data.error = "true";
+                data.status = "error";
+                data.code = "ElementNotFound";
+                data.message = "User with id " + req.params.id + " does not exist";
             } else {
+                data.status = "success"
+                data.code = "ResultsForSearch"
                 data.data = users;
+                data.count = users.length;
             }
             res.send(data);
             next();
