d27e128537cee042f41ca609a38c0e5974620331
Eder Toledo <me890506@gmail.com>
2016-11-14 22:00:57 -0600

    Se corrigieron las relaciones para evitar el renombramiento en plural y singular en las consultas

--- ExamWeb/Models/index.js
+++ ExamWeb/Models/index.js
@@ -10,101 +10,112 @@ var models = require('sequelize-auto-import')(sequelize, '');
 
 //Relación materias - grado
 models.ex_Materia.belongsTo(models.ex_Grado, {
-    foreignKey: 'ex_Grado_idGrado'
+    foreignKey: 'ex_Grado_idGrado',
+    as: 'grado'
 });
 
 models.ex_Grado.hasMany(models.ex_Materia, {
-    foreignKey: 'ex_Grado_idGrado'
+    foreignKey: 'ex_Grado_idGrado',
+    as: 'materia'
 });
 
 //Relacion usuarios - estado
 models.ex_User.belongsTo(models.ex_Estado, {
-    foreignKey: 'ex_Estado_idEstado'
+    foreignKey: 'ex_Estado_idEstado',
+    as: 'estado'
 });
 
 models.ex_Estado.hasMany(models.ex_User, {
-    foreignKey: 'ex_Estado_idEstado'
+    foreignKey: 'ex_Estado_idEstado',
+    as: 'user'
 });
 
 //Relacion datosAcademicos - usuario
 models.ex_DatosAcademicos.belongsTo(models.ex_User, {
-    foreignKey: 'ex_user_idUser'
+    foreignKey: 'ex_user_idUser',
+    as: 'user'
 });
 
 models.ex_User.hasOne(models.ex_DatosAcademicos, {
-    foreignKey: 'ex_user_idUser'
+    foreignKey: 'ex_user_idUser',
+    as: 'datosAcademicos'
 });
 
 //Relacion datosAcademicos - carrera
 models.ex_DatosAcademicos.belongsTo(models.ex_Carrera, {
-    foreignKey: 'ex_Carrera_idCarrera'
+    foreignKey: 'ex_Carrera_idCarrera',
+    as: 'carrera'
 });
 
 models.ex_Carrera.hasMany(models.ex_DatosAcademicos, {
-    foreignKey: 'ex_Carrera_idCarrera'
+    foreignKey: 'ex_Carrera_idCarrera',
+    as: 'datosAcademicos'
 });
 
 //Relacion datosAcademicos - gradoAcademico
 models.ex_DatosAcademicos.belongsTo(models.ex_GradoAcademico, {
-    foreignKey: 'ex_Grado_idGrado'
+    foreignKey: 'ex_Grado_idGrado',
+    as: 'gradoAcademico'
 });
 
 models.ex_GradoAcademico.hasMany(models.ex_DatosAcademicos, {
-    foreignKey: 'ex_Grado_idGrado'
+    foreignKey: 'ex_Grado_idGrado',
+    as: 'datosAcademicos'
 });
 
 //Relacion estados - pais
 models.ex_Estado.belongsTo(models.ex_Pais, {
-    foreignKey: 'ex_Pais_idpais'
+    foreignKey: 'ex_Pais_idpais',
+    as: 'pais'
 });
 
 models.ex_Pais.hasMany(models.ex_Estado, {
-    foreignKey: 'ex_Pais_idpais'
+    foreignKey: 'ex_Pais_idpais',
+    as: 'estado'
 });
 
 //Relacion examenes - usuario
 models.ex_Examen.belongsTo(models.ex_User, {
-    foreignKey: 'ex_User_idUser'
+    foreignKey: 'ex_User_idUser',
+    as: 'user'
 });
 
 models.ex_User.hasMany(models.ex_Examen, {
-    foreignKey: 'ex_User_idUser'
-});
-
-//Relacion examenes - periodo
-models.ex_Examen.belongsTo(models.ex_Periodo, {
-    foreignKey: 'ex_Periodo_idPeriodo'
-});
-
-models.ex_Periodo.hasMany(models.ex_Examen, {
-    foreignKey: 'ex_Periodo_idPeriodo'
+    foreignKey: 'ex_User_idUser',
+    as : 'examen'
 });
 
 //Relacion examenes - materia
 models.ex_Examen.belongsTo(models.ex_Materia, {
-    foreignKey: 'ex_Materia_idMateria'
+    foreignKey: 'ex_Materia_idMateria',
+    as: 'materia'
 });
 
 models.ex_Materia.hasMany(models.ex_Examen, {
-    foreignKey: 'ex_Materia_idMateria'
+    foreignKey: 'ex_Materia_idMateria',
+    as: 'examen'
 });
 
 //Relacion grados - nivel
 models.ex_Grado.belongsTo(models.ex_Nivel, {
-    foreignKey: 'ex_Nivel_idNivel'
+    foreignKey: 'ex_Nivel_idNivel',
+    as: 'nivel'
 });
 
 models.ex_Nivel.hasMany(models.ex_Grado, {
-    foreignKey: 'ex_Nivel_idNivel'
+    foreignKey: 'ex_Nivel_idNivel',
+    as: 'grado'
 });
 
 //Relacion periodos - grado
 models.ex_Periodo.belongsTo(models.ex_Grado, {
-    foreignKey: 'ex_Grado_idGrado'
+    foreignKey: 'ex_Grado_idGrado',
+    as: 'grado'
 });
 
 models.ex_Grado.hasMany(models.ex_Periodo, {
-    foreignKey: 'ex_Grado_idGrado'
+    foreignKey: 'ex_Grado_idGrado',
+    as: 'periodo'
 });
 
 
--- ExamWeb/Routes/usersRoute.js
+++ ExamWeb/Routes/usersRoute.js
@@ -3,7 +3,7 @@
 
     var sql = {};
     sql.where = { estatus: "Activo"};
-    sql.attributes = { exclude: ['createdAt', 'updatedAt', 'estatus', 'password', 'ex_Estado_idEstado'] };
+    sql.attributes = { exclude: ['createdAt', 'updatedAt', 'estatus', 'password'] };
 
     function allUsers(req, res, next) {
         db.ex_User.findAll(sql).then(function (users) {
@@ -31,38 +31,42 @@
                 if (relations[i] == "datosAcademicos") {
                     sql.include[i] = {};
                     sql.include[i].model = db.ex_DatosAcademicos;
+                    sql.include[i].as = 'datosAcademicos';
                     sql.include[i].attributes = { exclude: ['createdAt', 'updatedAt', 'ex_Grado_idGrado', 'ex_Carrera_idCarrera', 'ex_user_idUser'] };
                     sql.include[i].include = [];
                     sql.include[i].include[0] = {};
                     sql.include[i].include[0].model = db.ex_Carrera;
+                    sql.include[i].include[0].as = 'carrera';
                     sql.include[i].include[0].attributes = { exclude: ['createdAt', 'updatedAt'] };
                     sql.include[i].include[1] = {};
                     sql.include[i].include[1].model = db.ex_GradoAcademico;
+                    sql.include[i].include[1].as = 'gradoAcademico';
                     sql.include[i].include[1].attributes = { exclude: ['createdAt', 'updatedAt'] };
                 } else if (relations[i] == "estado") {
                     sql.include[i] = {};
                     sql.include[i].model = db.ex_Estado;
+                    sql.include[i].as = 'estado';
                     sql.include[i].attributes = { exclude: ['createdAt', 'updatedAt', 'ex_Pais_idpais'] };
                     sql.include[i].include = [];
                     sql.include[i].include[0] = {};
                     sql.include[i].include[0].model = db.ex_Pais;
+                    sql.include[i].include[0].as = 'pais';
                     sql.include[i].include[0].attributes = { exclude: ['createdAt', 'updatedAt'] };
                 } else if (relations[i] == "examen") {
                     sql.include[i] = {};
                     sql.include[i].model = db.ex_Examen;
+                    sql.include[i].as = 'examen'
                     sql.include[i].attributes = { exclude: ['createdAt', 'updatedAt', 'ex_Materia_idMateria', 'ex_periodo_idPeriodo', 'ex_User_iduser', 'estatus'] };
                     sql.include[i].include = [];
                     sql.include[i].include[0] = {};
-                    sql.include[i].include[0].model = db.ex_Periodo;
+                    sql.include[i].include[0].model = db.ex_Materia;
+                    sql.include[i].include[0].as = 'materia';
                     sql.include[i].include[0].attributes = { exclude: ['createdAt', 'updatedAt', 'estatus', 'ex_Grado_idGrado'] };
                     sql.include[i].include[0].include = [];
                     sql.include[i].include[0].include[0] = {};
                     sql.include[i].include[0].include[0].model = db.ex_Grado;
+                    sql.include[i].include[0].include[0].as = 'grado';
                     sql.include[i].include[0].include[0].attributes = { exclude: ['createdAt', 'updatedAt', 'ex_Nivel_idNivel'] };
-                    sql.include[i].include[1] = {};
-                    sql.include[i].include[1].model = db.ex_Materia;
-                    sql.include[i].include[1].attributes = { exclude: ['createdAt', 'updatedAt', 'estatus', 'ex_Grado_idGrado'] };
-                    sql.include[i].include[1].exclude = ['ex_User'];
                 } else {
                     sql = '';
                 }
